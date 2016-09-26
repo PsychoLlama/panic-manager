db38e4d7a8a3d0a6aa55301cefed24673c3accb1
Jesse Gibson <Jesse_Gibson@me.com>
2016-09-26 15:44:22 -0600

    Add remote manager support
    You can now start a manager server on one computer, connect to it from
    another computer, then remotely create clients. Now I've just gotta try
    it on windows...

--- src/Manager.js
+++ src/Manager.js
@@ -2,6 +2,7 @@
 
 var isArray = require('isarray');
 var spawn = require('./spawn-clients');
+var socket = require('./socket');
 
 /**
  * Validate a config object.
@@ -45,13 +46,61 @@ function validate (config) {
  * Manage panic clients in bulk, either locally or remotely.
  *
  * @class Manager
+ * @param {Mixed} remote - Either an http.Server to attach to,
+ * a URL to connect to, or `undefined` if there is no remote.
  */
-function Manager () {
+function Manager (remote) {
 
 	/** Allow usage without `new`. */
 	if (!(this instanceof Manager)) {
-		return new Manager();
+		return new Manager(remote);
 	}
+
+	/**
+	 * @property {Boolean} isRemote
+	 * Whether the manager is remote.
+	 */
+	this.isRemote = false;
+
+	/**
+	 * @property {Socket} socket
+	 * A socket.io instance, or `null` if using locally.
+	 */
+	this.socket = null;
+
+	/** If only using locally, stop here. */
+	if (!remote) {
+		return this;
+	}
+
+	/** If it's a URL, connect to it. */
+	if (typeof remote === 'string') {
+		this.isRemote = true;
+
+		/** Automatically postfix the manager scope. */
+		var url = remote + '/panic-manager';
+
+		/** Connect to the remote manager. */
+		this.socket = socket.client(url);
+
+		return this;
+	}
+
+	/** Listen for /panic-manager connections. */
+	var server = socket.server(remote).of('/panic-manager');
+
+	/** Listen for connections. */
+	server.on('connection', function (socket) {
+
+		/** Listen for start commands. */
+		socket.on('start', function (config) {
+			spawn(config);
+		});
+
+	});
+
+	this.socket = server;
+
 }
 
 Manager.prototype = {
@@ -68,8 +117,16 @@ Manager.prototype = {
 		/** May throw validation errors. */
 		validate(config);
 
-		/** Creates the requested clients. */
-		spawn(config);
+		if (this.isRemote) {
+
+			/** Spawn the clients remotely. */
+			this.socket.emit('start', config);
+		} else {
+
+			/** Spawn the clients locally. */
+			spawn(config);
+		}
+
 	},
 };
 
--- src/test.js
+++ src/test.js
@@ -1,12 +1,42 @@
 'use strict';
 const { describe, it, beforeEach, afterEach } = require('mocha');
+
+/* Spy setup */
 const expect = require('expect');
-const { spyOn } = expect;
+const { spyOn, createSpy } = expect;
+
+/* Child process spy */
 const ChildProcess = require('child_process');
-const spy = spyOn(ChildProcess, 'fork');
+const fork = spyOn(ChildProcess, 'fork');
+
+const sockets = require('./socket');
+const Emitter = require('events');
+const emitter = new Emitter();
+
+/* Socket spies */
+const socket = {
+	of: createSpy().andReturn(emitter),
+	server: spyOn(sockets, 'server'),
+	client: spyOn(sockets, 'client').andReturn(emitter),
+};
+
+socket.server.andReturn({
+	of: socket.of,
+});
+
+const { Server } = require('http');
 const Manager = require('./Manager');
 
-describe('A manager', () => {
+// Simple `.start()` config.
+const config = {
+	clients: [{ type: 'node' }],
+	panic: 'http://localhost:1234',
+};
+
+afterEach(() => fork.reset());
+afterEach(() => emitter.removeAllListeners());
+
+describe('A local manager', () => {
 
 	let manager;
 
@@ -14,11 +44,14 @@ describe('A manager', () => {
 		manager = new Manager();
 	});
 
-	afterEach(() => spy.reset());
-
 	it('should be constructable without `new`', () => {
 		const manager = Manager();
 		expect(manager).toBeA(Manager);
+	});
+
+	it('should declare itself as local', () => {
+		expect(manager.isRemote).toBe(false);
+		expect(manager.socket).toBe(null);
 	});
 
 	describe('`.start()` call', () => {
@@ -59,7 +92,96 @@ describe('A manager', () => {
 				clients: [{ type: 'node' }],
 				panic: 'url',
 			});
-			expect(spy).toHaveBeenCalled();
+			expect(fork).toHaveBeenCalled();
+		});
+
+	});
+
+});
+
+describe('A manager server', () => {
+
+	it('should declare itself as local', () => {
+		const manager = Manager(8080);
+		expect(manager.isRemote).toBe(false);
+	});
+
+	it('should attach to the passed server', () => {
+		const server = Server();
+		Manager(server);
+		expect(socket.server).toHaveBeenCalledWith(server);
+	});
+
+	it('should pass port numbers to socket.io', () => {
+		Manager(8080);
+		expect(socket.server).toHaveBeenCalledWith(8080);
+	});
+
+	it('should namespace to /panic-manager', () => {
+		Manager(8080);
+		expect(socket.of).toHaveBeenCalledWith('/panic-manager');
+	});
+
+	it('should spawn clients on socket "start" event', () => {
+		Manager(8080);
+
+		// Resolve to a fake socket.
+		emitter.emit('connection', emitter);
+
+		// It should be listening for "start" now.
+		emitter.emit('start', {
+			clients: [{ type: 'node' }],
+			panic: 'potato.com',
+		});
+
+		// child_process.fork should have been triggered.
+		expect(fork).toHaveBeenCalled();
+	});
+
+	describe('`.start()` call', () => {
+
+		it('should only invoke `spawn-clients`', () => {
+			const event = createSpy();
+			const manager = Manager(8080);
+
+			// Make sure "start" is never fired.
+			manager.socket.on('start', event);
+			manager.start(config);
+
+			expect(event).toNotHaveBeenCalled();
+			expect(fork).toHaveBeenCalled();
+		});
+
+	});
+
+});
+
+describe('A manager client', () => {
+
+	let client;
+
+	beforeEach(() => {
+		client = Manager('http://url.com');
+	});
+
+	it('should declare itself as remote', () => {
+		expect(client.isRemote).toBe(true);
+	});
+
+	describe('`.start()` call', () => {
+
+		it('should emit on the remote socket', () => {
+			const spy = createSpy();
+
+			client.socket.on('start', spy);
+			client.start(config);
+
+			expect(spy).toHaveBeenCalledWith(config);
+		});
+
+		it('should not call `fork`', () => {
+			client.start(config);
+			expect(fork).toNotHaveBeenCalled();
 		});
 
 	});
